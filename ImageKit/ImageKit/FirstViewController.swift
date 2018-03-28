27aa06b4d0a5e226e9d662d474a83abdfc2ea84d
ttionn <matt.tian@icloud.com>
2018-03-28 13:52:04 +0800

    Add GoldenGate image

---
+++ ImageKit/ImageKit/FirstViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FirstViewController.swift
+//  ImageKit
+//
+//  Created by Matt Tian on 28/03/2018.
+//  Copyright © 2018 TTSY. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imageView.backgroundColor = UIColor.fromHEX(string: "#EEEEEE")
+    }
+    
+    @IBAction func handleUpload(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        self.present(imagePickerController, animated: true)
+    }
+    
+}
+
+extension FirstViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        imageView.image = image
+        
+        dismiss(animated: true)
+    }
+}
---
+++ ImageKit/ImageKit/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  ImageKit
+//
+//  Created by Matt Tian on 28/03/2018.
+//  Copyright © 2018 TTSY. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.fromHEX(string: "#ABCDEF")
+    }
+
+}
